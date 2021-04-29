b96962c0347f8903789525c1419d1ce2d89361d0
mIPlug <luizfernandoluth@gmail.com>
2021-04-29 14:28:40 -0300

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,75 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	
+		private static Connection conn = null;
+		
+		public static Connection getConnection() {
+			if (conn == null) {
+				try {
+					Properties props = loadProperties();
+					String dburl = props.getProperty("dburl");
+					String url = dburl;
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					conn = DriverManager.getConnection(url, props);
+				} catch (SQLException e) {
+					throw new DbException(e.getMessage());	
+				} catch (ClassNotFoundException e) {
+					throw new DbException(e.getMessage());
+				}
+			}
+			return conn;
+		}
+		
+		public static void closeConnection() {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {	
+					throw new DbException(e.getMessage());
+				}
+			}
+		}
+		
+		
+	
+		private static Properties loadProperties() {
+			String properties = "db.properties";
+			try (FileInputStream fs = new FileInputStream(properties)) {
+				Properties props = new Properties();
+				props.load(fs);
+				return props;
+			} catch (IOException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		
+		public static void closeStatement(Statement st) {
+			if (st != null) {
+				try {
+					st.close();
+				} catch (SQLException e) {
+					throw new DbException(e.getMessage());
+				}
+			}
+		}
+		
+		public static void closeResultSet(ResultSet rs) {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					throw new DbException(e.getMessage());
+				}
+			}
+		}
+}

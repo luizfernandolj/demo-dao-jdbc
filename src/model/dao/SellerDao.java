d34c43b0868e90d11981c9bb04064bd188682184
mIPlug <luizfernandoluth@gmail.com>
2021-04-29 17:14:00 -0300

    findById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -9,6 +9,6 @@ public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
-	Seller findByIf(Integer id);
+	Seller findById(Integer id);
 	List<Seller> findAll();
 }

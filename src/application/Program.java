85b25a68455f913d8801d74d336804c5f25e0fec
mIPlug <luizfernandoluth@gmail.com>
2021-04-30 00:08:13 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -26,10 +27,15 @@ public class Program {
 		}
 		
 		System.out.println("=== TEST 3: seller findAll ====");
-		List<Seller> listall = sellerDao.findAll();
-		for (Seller obj : listall) {
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== TEST 4: seller insert ====");
+		Seller newseller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newseller);
+		System.out.println("Inserted! New id = " + newseller.getId());
 	}
 }
  
